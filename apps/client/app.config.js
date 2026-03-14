b250568313897aac6739a2539e9403ed60a9e1a1
Everett McIntire <emcintire@appliedmgt.com>
2026-03-14 14:31:27 -0400

    fixed icon

--- apps/client/app.config.js
+++ apps/client/app.config.js
@@ -1,57 +1,57 @@
-import "dotenv/config";
+import 'dotenv/config';
 
 export default {
   expo: {
-    name: "unCaged",
-    slug: "unCaged",
-    icon: "./src/assets/imgs/icon.png",
-    version: "10.0.0",
-    orientation: "portrait",
-    scheme: "uncaged",
-    platforms: ["ios", "android", "web"],
-    jsEngine: "hermes",
+    name: 'unCaged',
+    slug: 'unCaged',
+    icon: './src/assets/imgs/icon.png',
+    version: '10.1.0',
+    orientation: 'portrait',
+    scheme: 'uncaged',
+    platforms: ['ios', 'android', 'web'],
+    jsEngine: 'hermes',
     extra: {
       apiBaseUrl:  process.env.API_BASE_URL,
       eas: {
-        projectId: "8b66d794-89ac-4204-8dcd-3d8e025423fd",
+        projectId: '8b66d794-89ac-4204-8dcd-3d8e025423fd',
       },
     },
     splash: {
-      image: "./src/assets/imgs/icon.png",
-      resizeMode: "contain",
-      backgroundColor: "#000000",
+      image: './src/assets/imgs/icon.png',
+      resizeMode: 'contain',
+      backgroundColor: '#000000',
     },
     updates: {
-      url: "https://u.expo.dev/8b66d794-89ac-4204-8dcd-3d8e025423fd",
+      url: 'https://u.expo.dev/8b66d794-89ac-4204-8dcd-3d8e025423fd',
     },
     runtimeVersion: {
-      policy: "appVersion",
+      policy: 'appVersion',
     },
-    assetBundlePatterns: ["**/*"],
+    assetBundlePatterns: ['**/*'],
     ios: {
       supportsTablet: true,
-      bundleIdentifier: "uncaged.app",
-      googleServicesFile: "./GoogleService-Info.plist",
+      bundleIdentifier: 'uncaged.app',
+      googleServicesFile: './GoogleService-Info.plist',
       config: {},
     },
     android: {
-      package: "uncaged.app",
+      package: 'uncaged.app',
       versionCode: 11,
-      googleServicesFile: "./google-services.json",
+      googleServicesFile: './google-services.json',
       adaptiveIcon: {
-        foregroundImage: "./src/assets/imgs/icon.png",
-        backgroundColor: "#000000",
+        foregroundImage: './src/assets/imgs/icon-foreground.png',
+        backgroundColor: '#2c2d34',
       },
       config: {},
     },
     web: {
-      favicon: "./src/assets/imgs/icon.png",
+      favicon: './src/assets/imgs/icon.png',
     },
     plugins: [
-      "expo-font",
-      "expo-secure-store",
-      "@react-native-firebase/app",
-      "./plugins/withModularHeaders",
+      'expo-font',
+      'expo-secure-store',
+      '@react-native-firebase/app',
+      './plugins/withModularHeaders',
     ],
   },
 };
